dc27346237596034763cc628fc6d33f107b90a28
Peter Esselius <peter.esselius@pagero.com>
2021-10-07 09:31:47 +0200

    wip

--- flakeModules/outputs.nix
+++ flakeModules/outputs.nix
@@ -5,9 +5,22 @@ with lib.types;
 
 {
   options = {
-    outputs = mkOption {
-      default = { };
-      type = attrs;
+    outputs = {
+      apps = mkOption { type = attrs; };
+      checks = mkOption { type = attrs; };
+      defaultApp = mkOption { type = attrs; };
+      defaultPackage = mkOption { type = attrs; };
+      defaultTemplate = mkOption { type = attrs; };
+      devShell = mkOption { type = attrs; };
+      hydraJobs = mkOption { type = attrs; };
+      legacyPackages = mkOption { type = attrs; };
+      nixosConfigurations = mkOption { type = attrs; };
+      nixosModule = mkOption { type = attrs; };
+      nixosModules = mkOption { type = attrs; };
+      overlay = mkOption { type = attrs; };
+      overlays = mkOption { type = attrs; };
+      packages = mkOption { type = attrs; };
+      templates = mkOption { type = attrs; };
     };
   };
 }
